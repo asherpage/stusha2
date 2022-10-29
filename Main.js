7b033c63a833d5b270b6e429459627a7a17a4c1a
asherpage <110576723+asherpage@users.noreply.github.com>
2022-10-28 18:22:15 -0700

    10/28

--- Main.js
+++ Main.js
@@ -21,7 +21,7 @@ function getInfo() {
 	for(var i = 0; i < objPeople.length; i++) {
 		// check is user input matches username and password of a current index of the objPeople array
 		if(username == objPeople[i].username && password == objPeople[i].password) {
-            location.href = "admin.html";
+            location.href = "studentPage.html";
 			console.log(username + " is logged in!!!")
 			// stop the function if this is found to be true
 			return
@@ -30,8 +30,7 @@ function getInfo() {
     
 	console.log("incorrect username or password")
     document.getElementById('print').innerHTML
-                = 'Invalid Login';
-            console.log("no")
+                = 'INVALID LOGIN';
 
     
 }
