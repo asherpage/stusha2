a917663f71daa7c015ada6d482b8f72ea1b5df0e
asherpage <110576723+asherpage@users.noreply.github.com>
2023-01-19 18:22:24 -0700

    search

--- admin.js
+++ admin.js
@@ -35,7 +35,20 @@ function getInfo() {
  
 }
 //search fucntion for student info
-const data =[{firstName:"| Name: John", lastName:"Doe |", age:"Age:15", studentId:45},{studentId:78 ,firstName:"Merik", lastName:"Kuhn", age: "Age:17", }]
+const data =[
+	{studentId:1 , firstName:"John",lastName:"Doe",age:"14",grade: "9th"},
+{studentId:2 ,firstName:"Merik", lastName:"Kuhn", age: "17",grade: "11th"},
+{studentId:3 ,firstName:"Asher", lastName:"Page", age: "18",grade: "12th"},
+{studentId:4 ,firstName:"Andrew", lastName:"silaghi", age: "17",grade: "11th"},
+{studentId:5 ,firstName:"Anthony", lastName:"Lucas", age: "16",grade: "10th"},
+{studentId:6 ,firstName:"Jordan", lastName:"Reed", age: "16",grade: "10th"},
+{studentId:7 ,firstName:"Kyle", lastName:"Smith", age: "15",grade: "9th"},
+{studentId:8 ,firstName:"Blake", lastName:"Reed", age: "17",grade: "11th"},
+{studentId:9 ,firstName:"Emma", lastName:"Taylor", age: "18",grade: "12th"},
+{studentId:10 ,firstName:"Alexandra", lastName:"Reed", age: "17",grade: "11th"},
+{studentId:11 ,firstName:"Jadyn", lastName:"DeMarco", age: "16",grade: "10th"},
+{studentId:12 ,firstName:"Jaci", lastName:"Robinson", age: "17",grade: "11th"}
+]
 function search(srch){
 	console.log(srch)
 	for(let k = 0; k < data.length; k++ ){
@@ -43,7 +56,13 @@ function search(srch){
 		if(data[k].studentId==Number(srch)){
 			console.log(data[k])
 			console.log(Number(srch))
-			document.getElementById("prnt").innerHTML=`<p>Id:${data[k].studentId}   ${data[k].firstName} ${data[k].lastName} ${data[k].age}</p>`
+			document.getElementById("prnt").innerHTML=`<p>Id: ${data[k].studentId} | Name: ${data[k].firstName} ${data[k].lastName} | Age: ${data[k].age} | Grade: ${data[k].grade}</p>`
+			
+		}
+				if(data[k].firstName==(srch)){
+			console.log(data[k])
+			console.log((srch))
+			document.getElementById("prnt").innerHTML=`<p>Id: ${data[k].studentId} | Name: ${data[k].firstName} ${data[k].lastName} | Age: ${data[k].age} | Grade: ${data[k].grade}</p>`
 			
 		}
 	}
